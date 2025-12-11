6094836c4bf47f62977f10d384c7a77892e45995
Candyiy <jbastaluego@gmail.com>
2025-12-11 16:27:14 -0400

    inicio

---
+++ src/test.py
@@ -0,0 +1,11 @@
+from app import Calculadora
+def test_sum_2_numeros():
+    assert Calculadora().sum(2,2) ==4
+#prueba unitaria comando instalar -> pip install pytest
+#pytest .\src\test.py
+#git init
+#git add .
+#git commit -m "inicio"
+#git branch -M main
+#git remote add origin "el link"
+#git push -u origin main
